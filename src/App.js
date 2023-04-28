b79f755e49f132daca7c68cfcd044b08d12d41fb
unknown <eecevah@gmail.com>
2023-04-28 15:47:20 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import logo from './logosLogoLight.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style={{display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'center', height:'100vh'}}>
+      <img src={logo} alt='logo' id='logo' width={750} height={200}></img>
+      <div id='text'>Yapım Aşamında</div>
     </div>
+    </>
   );
 }
 
